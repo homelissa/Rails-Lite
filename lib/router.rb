c243e251c0ac0b8becd16f5908b2f54135c846d6
Melissa Ho <homelissa93@gmail.com>
2019-01-06 15:48:54 -0800

    Router and Route classes

--- lib/router.rb
+++ lib/router.rb
@@ -2,10 +2,15 @@ class Route
   attr_reader :pattern, :http_method, :controller_class, :action_name
 
   def initialize(pattern, http_method, controller_class, action_name)
+    @pattern = pattern 
+    @http_method = http_method 
+    @controller_class = controller_class 
+    @action_name = action_name 
   end
 
   # checks if pattern matches path and method matches request method
   def matches?(req)
+    (http_method == req.request_method.downcase.to_sym) && !!(pattern =~ req.path)
   end
 
   # use pattern to pull out route params (save for later?)
@@ -18,10 +23,17 @@ class Router
   attr_reader :routes
 
   def initialize
+    @routes = []
   end
 
   # simply adds a new route to the list of routes
   def add_route(pattern, method, controller_class, action_name)
+    @routes << Route.new(
+      pattern,
+      method,
+      controller_class,
+      action_name
+    )
   end
 
   # evaluate the proc in the context of the instance
@@ -36,9 +48,17 @@ class Router
 
   # should return the route that matches this request
   def match(req)
+    routes.find { |route| route.matches?(req) }
   end
 
   # either throw 404 or call run on a matched route
   def run(req, res)
-  end
+    matching_route = match(req)
+
+    if matching_route.nil?
+      res.status = 404 
+    else 
+      matching_route.run(req, res)
+    end 
+  end 
 end
